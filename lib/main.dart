01e533bee50a190be5a6209d622abc20baba5da7
nasrinsamad19 <nasrinsamad19@gmail.com>
2021-02-09 12:18:34 +0530

    Added i18n sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,171 @@
 import 'package:flutter/material.dart';
-import 'package:hala_avis_car/screens/accident_report_ui.dart';
-import 'package:hala_avis_car/screens/home_ui.dart';
-import 'package:hala_avis_car/screens/loginui.dart';
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  AppLanguage appLanguage = AppLanguage();
+  await appLanguage.fetchLocale();
+  runApp(MyApp(
+    appLanguage: appLanguage,
+  ));
 }
 
+class AppLocalizations {
+  final Locale locale;
+
+  AppLocalizations(this.locale);
+
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate = _AppLocalizationsDelegate();
+
+  Map<String, String> _localizedStrings;
+
+  Future<bool> load() async {
+    String jsonString =
+    await rootBundle.loadString('assets/i18n/${locale.languageCode}.json');
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+
+    _localizedStrings = jsonMap.map((key, value) {
+      return MapEntry(key, value.toString());
+    });
+
+    return true;
+  }
+
+  String translate(String key) {
+    return _localizedStrings[key];
+  }
+}
+
+class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'ar'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    AppLocalizations localizations = new AppLocalizations(locale);
+    await localizations.load();
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(_AppLocalizationsDelegate old) => false;
+}
+
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final AppLanguage appLanguage;
+
+  MyApp({this.appLanguage});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: HomeUi()
+    return ChangeNotifierProvider<AppLanguage>(
+      create: (_) => appLanguage,
+      child: Consumer<AppLanguage>(builder: (context, model, child) {
+        return MaterialApp(
+          //locale: model.appLocal,
+          supportedLocales: [
+            Locale('en', 'US'),
+            Locale('ar', ''),
+          ],
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+
+          home: AppLang(),
+        );
+      }),
     );
-        }
+  }
+}
+
+class AppLang extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appLanguage = Provider.of<AppLanguage>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context).translate('title')),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              AppLocalizations.of(context).translate('Message'),
+              style: TextStyle(fontSize: 32),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    appLanguage.changeLanguage(Locale("en"));
+
+                  },
+                  child: Text('English'),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    appLanguage.changeLanguage(Locale("ar"));
+
+                  },
+                  child: Text('??????'),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AppLanguage extends ChangeNotifier {
+  Locale _appLocale = Locale('en');
+  Locale get appLocal => _appLocale ?? Locale("en");
+
+  fetchLocale() async {
+    var prefs = await SharedPreferences.getInstance();
+    if(prefs.getString('language_code')==null) {
+      _appLocale=Locale('en');
+      return null;
+    }
+    _appLocale=Locale(prefs.getString('language_code'));
+    return null;
+  }
+
+  void changeLanguage(Locale type) async {
+    var prefs = await SharedPreferences.getInstance();
+    if(_appLocale== type){
+      return;
+    }
+    if(type==Locale("ar")){
+      _appLocale=Locale('ar');
+      await prefs.setString('language_code', 'ar');
+      await prefs.setString('countryCode', '');
+    } else {
+      _appLocale=Locale('en');
+      await prefs.setString('language_code', 'en');
+      await prefs.setString('countryCode', 'US');
+    }
+    notifyListeners();
+  }
 }
