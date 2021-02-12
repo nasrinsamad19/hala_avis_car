e060f3b52f14b0356fdf8439497c79d5bf1d6fe4
nasrinsamad19 <nasrinsamad19@gmail.com>
2021-02-12 15:52:12 +0530

    Added loginpageSample

--- lib/main.dart
+++ lib/main.dart
@@ -48,37 +48,102 @@ class AppLang extends StatelessWidget {
     var appLanguage = Provider.of<AppLanguage>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppLocalizations.of(context).translate('title')),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              AppLocalizations.of(context).translate('Message'),
-              style: TextStyle(fontSize: 32),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                  onPressed: () {
-                    appLanguage.changeLanguage(Locale("en"));
-                  },
-                  child: Text('English'),
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    appLanguage.changeLanguage(Locale("ar"));
-                  },
-                  child: Text('العربي'),
-                )
-              ],
-            )
-          ],
+        title: Text(AppLocalizations.of(context).translate('button'),
         ),
+        backgroundColor: Colors.red,
       ),
+      body: Container(
+          padding: EdgeInsets.only(top: 25),
+          alignment: Alignment.topLeft,
+          child: Column(
+            children: <Widget>[
+              Text(
+                AppLocalizations.of(context).translate('title'),
+                style: TextStyle(fontSize: 72),
+              ),
+              Container(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                padding: EdgeInsets.only(left: 20,top: 20,right: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      AppLocalizations.of(context).translate('message'),
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Container(
+                      height: 5,
+                    ),
+                    Text(
+                      AppLocalizations.of(context).translate('Message2'),
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ],
+                ),
+
+              ),
+              Container(
+                height: 20,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 20,top: 20,right: 20),
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      AppLocalizations.of(context).translate('txt1'),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 20,right: 20),
+                alignment: Alignment.topLeft,
+                child:TextFormField(
+                  decoration: InputDecoration(
+                      labelText: AppLocalizations.of(context).translate('hint1')
+                  ),
+                ),
+              ),
+
+              Container(
+                height: 50,
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text(AppLocalizations.of(context).translate('button'),
+                  style: TextStyle(fontSize: 15),),
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () {
+                      appLanguage.changeLanguage(Locale("en"));
+                    },
+                    child: Text('English'),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      appLanguage.changeLanguage(Locale("ar"));
+                    },
+                    child: Text('العربي'),
+                  )
+                ],
+              ),
+
+            ],
+          ),
+        ),
     );
   }
 }
